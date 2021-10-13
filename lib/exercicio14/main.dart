a6d2558a2033340716cbf332daa5eeec20124464
Marcos Gabriel Ribeiro Souto <marcosgrsouto@gmail.com>
2021-10-13 18:45:39 -0300

    Exercicio 14 and Project Structure refactor

---
+++ lib/exercicio14/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_usabilidade/exercicio14/rota/rotas.dart';
+
+void main() => runApp(
+      MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => PrimeiraTela(),
+          '/segunda': (context) => SegundaTela(),
+          '/terceira': (context) => TerceiraTela(),
+        },
+      ),
+    );
